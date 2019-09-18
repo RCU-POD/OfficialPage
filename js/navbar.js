b62995832df4b8da384d98b062d1604d7e5b9c21
KattiaOlmedo27 <kattia.olmedo@gmail.com>
2019-09-18 18:29:51 -0500

    RCU-WS-001-NavbarUpdate

--- js/navbar.js
+++ js/navbar.js
@@ -1,42 +1,19 @@
 $(document).ready(function () {
-    var link1 = document.querySelector('.link1')
-    var link2 = document.querySelector('.link2')
-    var link3 = document.querySelector('.link3')
-    var link4 = document.querySelector('.link4')
-    var link5 = document.querySelector('.link5')
-    var link6 = document.querySelector('.link6')
-    var link7 = document.querySelector('.link7')
-    var link8 = document.querySelector('.link8')
-  
+  /* For Standar Menu*/
   window.onscroll = function() {scrollFunction()};
   function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
       navbar.style.top = "0px";
       navbar.style.backgroundColor = '#FFFFFF';
-      link1.style.color ='#2E2D2C';
-      link2.style.color ='#2E2D2C';
-      link3.style.color ='#2E2D2C';
-      link4.style.color ='#2E2D2C';
-      link5.style.color ='#2E2D2C';
-      link6.style.color ='#2E2D2C';
-      link7.style.color ='#2E2D2C';
-      link8.style.color ='#2E2D2C';
-  
+      $(".nav-link").css("color", "#000000");
     } else {
       navbar.style.position = 'fixed';
-      navbar.style.backgroundColor = '#2E2D2C';
-      link1.style.color ='#FFFFFF';
-      link2.style.color ='#FFFFFF';
-      link3.style.color ='#FFFFFF';
-      link4.style.color ='#FFFFFF';
-      link5.style.color ='#FFFFFF';
-      link6.style.color ='#FFFFFF';
-      link7.style.color ='#FFFFFF';
-      link8.style.color ='#FFFFFF';
+      navbar.style.backgroundColor = '#000000';
+      $(".nav-link").css("color", "#FFFFFF");
     }
-  }
-  
-  
+  }  
+    
+  /* For Mobile Menu*/
     $('.nav-button').click(function () {
       $('body').toggleClass('nav-open');
     });
